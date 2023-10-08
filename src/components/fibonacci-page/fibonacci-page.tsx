62e8c68b4f1b101023792a42356c2f30ea1967d5
Mishe4ka14 <mishechka.14@yandex.ru>
2023-10-08 20:38:20 +0300

    add fibonacci start page

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from './fibbonacci-page.module.css'
+import { Button } from "../ui/button/button";
+import { useInputHandlers } from "../../hooks/use-input";
+import { useState } from "react";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 
 export const FibonacciPage: React.FC = () => {
+
+  const  {values, handleInputChange} = useInputHandlers({
+    num: ''
+  })
+
+const onClick = (e: React.FormEvent<HTMLFormElement>) => {
+  e.preventDefault()
+  console.log(values.value)
+} 
+
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
-     
+      <form className={styles.box} onSubmit={onClick}>
+        <Input width={400} maxLength={19} isLimitText onChange={handleInputChange}/>
+        <Button text="Развернуть" type="submit"/>
+      </form>
+      <div className={styles.container}>
+
+      </div>
     </SolutionLayout>
   );
 };
